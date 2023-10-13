a880400e2d36a8ad0fa81d417353ee6e52e265e7
Hasib <hasibhosen7612@gmail.com>
2023-10-13 22:01:51 +0600

    Progress Bar / Updated All

--- progress-bar/script.js
+++ progress-bar/script.js
@@ -1,10 +1,4 @@
-function randomIntFromInterval(min, max) {
-  // min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-const rndInt = randomIntFromInterval(1, 99);
-
-function progressWithoutValue(i) {
+function barWithoutValue(i) {
   let progress = document.querySelector(".progress");
   let intervalID = setInterval(move, 20);
   function move() {
@@ -17,7 +11,7 @@ function progressWithoutValue(i) {
   }
 }
 
-function progressWithValue(i) {
+function barWithValue(i) {
   let progress = document.querySelector(".progress-value");
   let intervalID = setInterval(move, 20);
   function move() {
@@ -27,6 +21,35 @@ function progressWithValue(i) {
       i++;
       progress.style.width = i + "%";
       progress.textContent = i + "%";
+    }
+  }
+}
+
+function circleWithoutValue(i) {
+  let progress = document.querySelector(".circle-progress-bar");
+  let intervalID = setInterval(move, 20);
+
+  function move() {
+    if (i >= 100) {
+      clearInterval(intervalID);
+    } else {
+      i++;
+      progress.style.background = `radial-gradient(closest-side, white 79%, transparent 80% 100%), conic-gradient(#ff69b4 ${i}%, pink 0)`;
+    }
+  }
+}
+
+function circleWithValue(i) {
+  let progress = document.querySelector(".circle-progress-bar-value");
+  let intervalID = setInterval(move, 20);
+
+  function move() {
+    if (i >= 100) {
+      clearInterval(intervalID);
+    } else {
+      i++;
+      progress.style.background = `radial-gradient(closest-side, white 79%, transparent 80% 100%), conic-gradient(#ff69b4 ${i}%, pink 0)`;
+      progress.style.setProperty("--before-content", `'${i}%'`);
     }
   }
 }
